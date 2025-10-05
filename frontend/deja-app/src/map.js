5f342e95081cc725ff69a413f0a399972f7a0a18
altaf-creator <dev@altafcreator.com>
2025-10-05 22:21:37 +0800

    actually finished impact. handled no effect case.

--- frontend/deja-app/src/map.js
+++ frontend/deja-app/src/map.js
@@ -10,8 +10,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([lat, lon]).addTo(map);
-
 const data = {
 	L0: parseFloat(params.get("estimated_maximum_diameter")),
 	Ui: 2000.0,
@@ -29,12 +27,6 @@ fetch("https://dejaapi.altafcreator.com/impact", {
 }).then(res => res.json()).then(result => {
 	impact = result;
 	console.log(impact)
-	var circle = L.circle([lat, lon], {
-		color: 'red',
-		fillColor: '#f03',
-		fillOpacity: 0.5,
-		radius: impact.crater_diamater / 2,
-	}).addTo(map);
 
 	document.getElementById("name").innerHTML = params.get("name")
 
@@ -46,19 +38,39 @@ fetch("https://dejaapi.altafcreator.com/impact", {
 	document.getElementById("tnt").innerHTML = formatNum(impact.E0 / (4.184*10**15)) + "&nbsp;megatons"
 	document.getElementById("hiroshima").innerHTML = formatNum(impact.E0 / (15*(4.184*10**15))) + "&nbsp;hiroshima bombs"
 
-	document.getElementById("craterdia").innerHTML = formatNum(impact.crater_diamater) + "&nbsp;m"
-	document.getElementById("craterdep").innerHTML = formatNum(impact.crater_depth) + "&nbsp;m"
+	if (!impact.crater_diamater) {
+		console.log(document.getElementById("noeff"))
+		document.getElementById("noeff").style.display = "block";
+		document.getElementById("eff").style.display = "none";
+	} else {
+		var marker = L.marker([lat, lon]).addTo(map);
 
-	document.getElementById("thermal").innerHTML = formatNum(parseFloat(impact.r_effects["1"].thermal_exposure) / 10**6) + "&nbspMJ⁄m²";
-	document.getElementById("mmi").innerHTML = impact.r_effects["1"].effective_mmi;
-	document.getElementById("wind").innerHTML = formatNum(parseFloat(impact.r_effects["1"].peak_wind_vel)) + "&nbspm⁄s";
-	document.getElementById("blast").innerHTML = formatNum(parseFloat(impact.r_effects["1"].surface_blast) / 10**6) + "&nbspMPa";
+		var circle = L.circle([lat, lon], {
+			color: 'red',
+			fillColor: '#f03',
+			fillOpacity: 0.5,
+			radius: impact.crater_diamater / 2,
+		}).addTo(map);
+
+		document.getElementById("energyim").innerHTML = formatNum(impact.E_ground / 10**12) + "&nbsp;TJ"
+		document.getElementById("tntim").innerHTML = formatNum(impact.E_ground / (4.184*10**15)) + "&nbsp;megatons"
+		document.getElementById("hiroshimaim").innerHTML = formatNum(impact.E_ground / (15*(4.184*10**15))) + "&nbsp;hiroshima bombs"
+		document.getElementById("energyair").innerHTML = formatNum(impact.E_air / 10**12) + "&nbsp;TJ"
 	
-	fetch(`https://lobster-app-bhpix.ondigitalocean.app/?lat=${params.get("lat")}&lng=${params.get("lon")}&radii=${Math.round(impact.crater_diamater / 2)}`)
-		.then((response) => response.json()).then(result => {
-			console.log(result)
-			document.getElementById("population").innerHTML = formatNum(parseFloat(result.populations[0]));
-		})
+		document.getElementById("craterdia").innerHTML = formatNum(impact.crater_diamater) + "&nbsp;m"
+		document.getElementById("craterdep").innerHTML = formatNum(impact.crater_depth) + "&nbsp;m"
+	
+		document.getElementById("thermal").innerHTML = formatNum(parseFloat(impact.r_effects["1"].thermal_exposure) / 10**6) + "&nbspMJ⁄m²";
+		document.getElementById("mmi").innerHTML = impact.r_effects["1"].effective_mmi;
+		document.getElementById("wind").innerHTML = formatNum(parseFloat(impact.r_effects["1"].peak_wind_vel)) + "&nbspm⁄s";
+		document.getElementById("blast").innerHTML = formatNum(parseFloat(impact.r_effects["1"].surface_blast) / 10**6) + "&nbspMPa";
+		
+		fetch(`https://lobster-app-bhpix.ondigitalocean.app/?lat=${params.get("lat")}&lng=${params.get("lon")}&radii=${Math.round(impact.crater_diamater / 2)}`)
+			.then((response) => response.json()).then(result => {
+				console.log(result)
+				document.getElementById("population").innerHTML = formatNum(parseFloat(result.populations[0]));
+			})
+	}
   
 	document.getElementById("chat1").classList.add("opacity-100");
 	document.getElementById("chat2").classList.add("opacity-100");
